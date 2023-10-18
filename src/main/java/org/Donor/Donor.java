e3c3663ad2f2e5c452501cf7be8f6d3f375fdd5e
vj_man26 <p.vijayroshan@gmail.com>
2023-10-18 13:18:40 +0530

    login bug fix

--- src/main/java/org/Donor/Donor.java
+++ src/main/java/org/Donor/Donor.java
@@ -43,7 +43,7 @@ public class Donor {
                 char[] passwordChars = passwordField.getPassword();
                 String password = new String(passwordChars);
 
-                if (1000 <= username && username < 10000){
+                if (!(1000 <= username && username < 10000)){
                     JOptionPane.showMessageDialog(frame, "Donor ID must be a 4-digit number");
                 } else if (DBConnections.isValidLogin(username, password)) {
                     // Close the login window
